641d9c21e3df5bb493abc2ead18c371322a75eda
Yousef27h <yousef_salem97@yahoo.com>
2021-05-08 22:19:09 +0300

    lab10

--- movies.js
+++ movies.js
@@ -3,14 +3,23 @@ const superagent = require("superagent");
 require("dotenv").config();
 let MOVIE_API_KEY = process.env.MOVIE_API_KEY;
 
+const inMemory = {};
 function moviesFun(req, res) {
   try {
-    const movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${req.query.query}`;
+    const movieName = req.query.query;
+    const movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${movieName}`;
     superagent.get(movieUrl).then((movieData) => {
-      const arrOfMovies = movieData.body.results.map(
-        (movieData) => new MovieDb(movieData)
-      );
-      res.send(arrOfMovies);
+      if (inMemory[movieName] !== undefined) {
+        // console.log("we got our data from memory!")
+        res.send(inMemory[movieName]);
+      } else {
+        // console.log("Got data from API!")
+        const arrOfMovies = movieData.body.results.map(
+          (movieData) => new MovieDb(movieData)
+        );
+        inMemory[movieName] = arrOfMovies;
+        res.send(arrOfMovies);
+      }
     });
   } catch (error) {
     console.log(error);
